4e8db8f2ee841da17ad9d12b670fae9fb87abe0b
VivekSharma-32 <50068155+VivekSharma-32@users.noreply.github.com>
2023-05-17 13:25:42 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const userRoute = require("./src/routes/user");
 const authRoute = require("./src/routes/auth");
+const productRoute = require("./src/routes/product");
+const cartRoute = require("./src/routes/cart");
+const orderRoute = require("./src/routes/order");
+const stripeRoute = require("./src/routes/stripe");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -12,10 +17,15 @@ mongoose
   .then(() => console.log("DB Connected Successfully!"))
   .catch((err) => console.log(err));
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/stripe", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is up and running on port: ${process.env.PORT}`);
